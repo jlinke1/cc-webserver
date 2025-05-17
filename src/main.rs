e456c303ab6da4d7b37d1e6b4fb338c781470e8d
jlinke1 <jlinke@flex-power.energy>
2025-05-17 14:44:38 +0200

    write status line

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io::{self, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,10 +15,20 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    let status_line = b"HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(status_line)?;
+
+    stream.flush()?;
+
+    Ok(())
 }
